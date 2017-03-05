795291042934b80a61e0882645b3816eb3a36334
apoorva <apoorva.maheshwari@sjsu.edu>
2017-03-04 23:33:23 -0800

    1. ParserInput updated to take input from console. 2. Constructor for parser.

--- src/UMLparser/ParseInput.java
+++ src/UMLparser/ParseInput.java
@@ -1,5 +1,9 @@
 package UMLparser;
 
 public class ParseInput {
-
+	public static void main (String [] args) {
+		if(args[0].equals("class")) {
+			Parser p = new Parser(args[1], args[2]);
+		}
+	}
 }
--- src/UMLparser/Parser.java
+++ src/UMLparser/Parser.java
@@ -1,5 +1,13 @@
 package UMLparser;
 
 public class Parser {
-
+	private String inPath;
+	private String outPath;
+	
+	public Parser(String inPath, String outPath) {
+		this.inPath = inPath;
+		this.outPath = outPath;
+	}
+	
+	
 }
