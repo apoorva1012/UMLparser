6684eb7340f7e2ce57f2560394531f017fb040d8
apoorva <apoorva.maheshwari@sjsu.edu>
2017-03-25 22:18:48 -0700

    parseAdditions added

--- src/UMLparser/Parser.java
+++ src/UMLparser/Parser.java
@@ -15,6 +15,11 @@ import net.sourceforge.plantuml.StringUtils;
 public class Parser {
 	private String inPath;
 	private String outPath;
+
+    HashMap<String, Boolean> map;
+    HashMap<String, String> mapClassConn;
+    String yumlCode;
+    ArrayList<CompilationUnit> cuArray;
 	
 	public Parser(String inPath, String outPath) {
 		this.inPath = inPath;
@@ -43,4 +48,22 @@ public class Parser {
         return result;
     }
 	
+	private String parseAdditions() {
+        String result = "";
+        Set<String> keys = mapClassConn.keySet(); // get all keys
+        for (String i : keys) {
+            String[] classes = i.split("-");
+            if (map.get(classes[0]))
+                result += "[<<interface>>;" + classes[0] + "]";
+            else
+                result += "[" + classes[0] + "]";
+            result += mapClassConn.get(i); // Add connection
+            if (map.get(classes[1]))
+                result += "[<<interface>>;" + classes[1] + "]";
+            else
+                result += "[" + classes[1] + "]";
+            result += ",";
+        }
+        return result;
+    }
 }
