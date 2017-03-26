ab41dfbdd2013ecee4b0b9f0023b97fd2a54fa8c
apoorva <apoorva.maheshwari@sjsu.edu>
2017-03-25 22:42:12 -0700

    removed redundancies

--- src/UMLparser/Parser.java
+++ src/UMLparser/Parser.java
@@ -33,7 +33,7 @@ public class Parser {
         cuArray = getCuArray(inPath);
         buildMap(cuArray);
         for (CompilationUnit cu : cuArray)
-            yumlCode += parse(cu);
+            yumlCode += parser(cu);
         yumlCode += parseAdditions();
         yumlCode = yumlCodeUniquer(yumlCode);
         System.out.println("Unique Code: " + yumlCode);
