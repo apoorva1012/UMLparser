992602b55ae4373861b36257b0c034e9767c8e4e
apoorva <apoorva.maheshwari@sjsu.edu>
2017-04-08 23:08:16 -0700

    updated CreateImage to add yUML dependency

--- src/UMLparser/CreateImage.java
+++ src/UMLparser/CreateImage.java
@@ -1,5 +1,17 @@
 package UMLparser;
 
-public class CreateImage {
+import java.net.*;
+import java.io.*;
 
+public class CreateImage {
+	public static void generatePNG(String grammar, String outPath) throws MalformedURLException {
+		String webLink = null;
+		try {
+			webLink = "https://yuml.me/diagram/plain/class/" + URLEncoder.encode(grammar, "UTF-8") + ".png";
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        URL url = new URL(webLink);
+	}
 }
