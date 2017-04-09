1a0a5000506995a9cc522e36af57eff172bccdc3
apoorva <apoorva.maheshwari@sjsu.edu>
2017-04-08 23:31:25 -0700

    Added an error condition to CreateImage

--- src/UMLparser/CreateImage.java
+++ src/UMLparser/CreateImage.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class CreateImage {
 	public static void generatePNG(String grammar, String outPath) throws MalformedURLException {
 		String webLink = null;
-		try {
+		
 			webLink = "https://yuml.me/diagram/plain/class/" + URLEncoder.encode(grammar, "UTF-8") + ".png";
 			URL url = new URL(webLink);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -17,10 +17,7 @@ public class CreateImage {
                 throw new RuntimeException(
                         "Failed : HTTP error code : " + conn.getResponseCode());
             }
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
         
 	}
 }
