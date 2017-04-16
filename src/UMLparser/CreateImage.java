58ae2ac0e410918d34e47ce0b792691f638d2ca8
apoorva <apoorva.maheshwari@sjsu.edu>
2017-04-15 23:21:33 -0700

    update CreateImage class

--- src/UMLparser/CreateImage.java
+++ src/UMLparser/CreateImage.java
@@ -4,20 +4,35 @@ import java.net.*;
 import java.io.*;
 
 public class CreateImage {
-	public static void generatePNG(String grammar, String outPath) throws IOException {
-		String webLink = null;
-		
-			webLink = "https://yuml.me/diagram/plain/class/" + URLEncoder.encode(grammar, "UTF-8") + ".png";
-			URL url = new URL(webLink);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+    public static Boolean generatePNG(String grammar, String outPath) {
+
+        try {
+
+            String webLink = "https://yuml.me/diagram/plain/class/" + URLEncoder.encode(grammar, "UTF-8") + ".png";
+            URL url = new URL(webLink);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
-            
+
             if (conn.getResponseCode() != 200) {
                 throw new RuntimeException(
                         "Failed : HTTP error code : " + conn.getResponseCode());
             }
-		
-        
-	}
+            OutputStream outputStream = new FileOutputStream(new File(outPath));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = conn.getInputStream().read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+            outputStream.close();
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
