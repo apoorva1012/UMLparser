eca5fc57db8bfb0c4d7784de793712e345857e79
apoorva <apoorva.maheshwari@sjsu.edu>
2017-04-15 23:35:08 -0700

    update the local path in ParseInput class

--- src/UMLparser/ParseInput.java
+++ src/UMLparser/ParseInput.java
@@ -3,7 +3,7 @@ package UMLparser;
 public class ParseInput {
 	public static void main(String[] args) throws Exception {
 
-        Parser pe = new Parser("/home/techmint/Downloads/MyUMLParser/src/test/java/classDiagramTest1", "/home/techmint/Downloads/MyUMLParser/src/sampleOutput/classDiagramTest1");
+        Parser pe = new Parser("/Users/apoorva/Documents/workspace/UMLParser/src/tests/uml-parser-test-1", "/Users/apoorva/Documents/workspace/UMLParser/src/tests/uml-parser-test-1");
         pe.start();
 
 //        SequenceParser pse = new SequenceParser("/home/techmint/Downloads/MyUMLParser/src/test/java/sequenceDiagramTest1", "Customer", "depositMoney", "/home/techmint/Downloads/MyUMLParser/src/sampleOutput/seq");
