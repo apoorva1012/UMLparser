a9ad20d577b9e4b0ae255fc356d23e6e39420505
apoorva <apoorva.maheshwari@sjsu.edu>
2017-04-15 23:31:33 -0700

    comment the sequence parser section

--- src/UMLparser/ParseInput.java
+++ src/UMLparser/ParseInput.java
@@ -1,9 +1,13 @@
 package UMLparser;
 
 public class ParseInput {
-	public static void main (String [] args) {
-		if(args[0].equals("class")) {
-			Parser pe = new Parser(args[1], args[2]);
-		}
+	public static void main(String[] args) throws Exception {
+
+        Parser pe = new Parser("/home/techmint/Downloads/MyUMLParser/src/test/java/classDiagramTest1", "/home/techmint/Downloads/MyUMLParser/src/sampleOutput/classDiagramTest1");
+        pe.start();
+
+//        SequenceParser pse = new SequenceParser("/home/techmint/Downloads/MyUMLParser/src/test/java/sequenceDiagramTest1", "Customer", "depositMoney", "/home/techmint/Downloads/MyUMLParser/src/sampleOutput/seq");
+//        pse.start();
+//}		
 	}
 }
