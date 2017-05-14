49fb6a54b0e61979f60e0c3d85c1967988e4d2e8
apoorva <apoorva.maheshwari@sjsu.edu>
2017-05-13 21:18:09 -0700

    final_commit

--- src/UMLparser/ParseInput.java
+++ src/UMLparser/ParseInput.java
@@ -3,11 +3,11 @@ package UMLparser;
 public class ParseInput {
 	public static void main(String[] args) throws Exception {
 
-        Parser pe = new Parser("/Users/apoorva/Documents/workspace/UMLParser/src/tests/uml-parser-test-1", "/Users/apoorva/Documents/workspace/UMLParser/src/tests/uml-parser-test-1");
+        Parser pe = new Parser("/Users/apoorva/Documents/workspace/UMLParser/tests/uml-parser-test-4", "/Users/apoorva/Downloads/amazingrace/result7");
         pe.start();
 
 //        SequenceParser pse = new SequenceParser("/home/techmint/Downloads/MyUMLParser/src/test/java/sequenceDiagramTest1", "Customer", "depositMoney", "/home/techmint/Downloads/MyUMLParser/src/sampleOutput/seq");
 //        pse.start();
-//}		
+//		
 	}
 }
