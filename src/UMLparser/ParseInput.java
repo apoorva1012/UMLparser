1b021aabeaab6603e3877be1f833913f4a3beee3
apoorva <apoorva.maheshwari@sjsu.edu>
2017-05-13 23:34:13 -0700

    test

--- src/UMLparser/ParseInput.java
+++ src/UMLparser/ParseInput.java
@@ -13,15 +13,15 @@ public class ParseInput {
                 c.start();
                 break;
             case 's':
-                SequenceParser s = new SequenceParser(args[1], "Customer", "depositMoney", args[2]);
+                SequenceParser s = new SequenceParser(args[1], "Main", "main", args[2]);
                 s.start();
                 break;
             default:
                 System.out.println("Please use below format for input");
                 System.out.println("For class diagram:");
-                System.out.println("java -jar CMPE202_jar -c <absolute input path in double quotes> <absolute output path in double quotes>");
+                System.out.println("java -jar CMPE202jar.jar -c <absolute input path in double quotes> <absolute output path in double quotes>");
                 System.out.println("For sequence diagram:");
-                System.out.println("java -jar CMPE202_jar -s <absolute input path in double quotes> <in Class name> <out Class name> <absolute output path in double quotes>");
+                System.out.println("java -jar CMPE202jar.jar -s <absolute input path in double quotes> <in Class name> <out Class name> <absolute output path in double quotes>");
         }
     }	
 }
